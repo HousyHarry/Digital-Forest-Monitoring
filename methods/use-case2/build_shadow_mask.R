4b2b424411a7d96cbef9c088a5677241707ffdde
Harry <daniel.h.peter@gmail.com>
2021-07-01 10:59:40 +0200

    load packages

--- methods/use-case2/build_shadow_mask.R
+++ methods/use-case2/build_shadow_mask.R
@@ -2,4 +2,10 @@
 # Function for automatic calculation of a shadow Mask
 ############################################################
 
-build_shadow_mask = function(){}
+build_shadow_mask = function(){
+
+  # load packages
+  library(raster)
+  library(doParallel)
+  library(rgrass7)
+}
